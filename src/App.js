4297397a37d8db8d64c4c05cc3379f1c58b9e4ef
rohit1250rathod <rohitrathodse2024@gmail.com>
2023-06-08 12:11:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Propelleraero Website Link
         </a>
       </header>
     </div>
